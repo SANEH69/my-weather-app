3d7ddcf64b6d374266a77d7beaf6c0643681ceea
SANEH69 <sanehthandoh02@gmail.com>
2024-11-05 22:33:16 +0200

    modified my HTML and JavaScript

--- src/function.js
+++ src/function.js
@@ -1,12 +1,24 @@
 let now = new Date()
 
-let .date = document.querySelector(".date");
+let span = document.querySelector("span");
 
-let date =now.getDate();
+let date = now.getDate();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
 
-.date.innerHTML = `${day} ${hours}:${minutes}`; 
+span.innerHTML = `${day} ${hours}:${minutes}`;
+
+function search(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#seach-input-text");
+
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `search for ${searchInput.value}`;
+    console.log(h1);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener('submit', search);
