b3babf2c47f7bf2f3b6791d2f8e3dfd1733bf3d7
SANEH69 <sanehthandoh02@gmail.com>
2024-11-07 23:32:08 +0200

    Added axios to my JavaScript and API

--- src/function.js
+++ src/function.js
@@ -1,3 +1,22 @@
+function displayTemperature(response) {
+    let tempElement = document.querySelector("#current-temp");
+    let temperature = Math.round(response.data.temperature.current);
+    h1.innerHTML = document.querySelector("h1");
+    h1 = response.data.city;
+    tempElement.innerHTML = temperature;
+}
+function searchForm(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input-text");
+    cityElement.innerHTML = document.querySelector("#current-city");
+    cityElement = searchInput.value;
+
+    let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayTemperature);
+}
+
 let now = new Date()
 
 let span = document.querySelector("span");
@@ -10,13 +29,3 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 let day = days[now.getDay()];
 
 span.innerHTML = `${day} ${hours}:${minutes}`;
-
-function searchForm(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input-text");
-    let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInput.value;
-}
-
-let formElement = document.querySelector("#search-form");
-formElement.addEventListener("submit", searchForm);
