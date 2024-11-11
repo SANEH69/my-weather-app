cb0884301849c7a7ecde775713552291360fafd3
SANEH69 <sanehthandoh02@gmail.com>
2024-11-11 23:15:44 +0200

    Modified my JavaScript into displaying real time date and time

--- src/function.js
+++ src/function.js
@@ -1,22 +1,31 @@
-let now = new Date()
+function formatDate(date) {
+let hours = date.getHours();
+let minutes = date.getMinutes();
 
-let span = document.querySelector("span");
-
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[now.getDay()];
-
-span.innerHTML = `${day} ${hours}:${minutes}`;
+let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+let day = days[date.getDay()];
+if (minutes < 10) {
+    minutes = `0${minutes}`; 
+}
+return `${day} ${hours}:${minutes}`;
+}
 
 function displayTemperature(response) {
     let tempElement = document.querySelector("#current-temp");
     let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let dayElement = document.querySelector("#day");
+    let date = new Date(response.data.time * 1000);
 
+    console.log(date);
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    dayElement.innerHTML = formatDate(date);
     tempElement.innerHTML = `${temperature}°C`;
 }
 
